dff52664f85d463935ad5d0b2cd94ffb97648f23
IVAN\idoli <idolibasic@outlook.com>
2023-03-14 22:22:01 +0100

    Minimal API initial commit

---
+++ UserDB/obj/Debug/postdeploy.sql
@@ -0,0 +1,10 @@
+﻿IF NOT EXISTS (SELECT 1 FROM dbo.[User])
+BEGIN
+	INSERT INTO dbo.[User] (FirstName, LastName)
+	VALUES
+	('Tim', 'Corey'),
+	('Sue', 'Storm'),
+	('John', 'Smith'),
+	('Mary', 'Jones');
+END
+GO
